0a094c47367196c9a3f13c8e23bc94b4d28a5a3b
Carlos Manuel <carlosm.matros@educa.jcyl.es>
2024-10-16 13:08:29 +0200

    new print methods and better code explanation

--- Computer.java
+++ Computer.java
@@ -1,25 +1,27 @@
 public class Computer {
+    //declaring class variables
     private String cpuId;
     private int ramVersion;    
     private boolean isOn;
-    
+    //Constructor
+    //initialation of class variables
     Computer (String cpuId, int ramVersion) {
         this.cpuId = cpuId;
         this.ramVersion = ramVersion;
+        //always on false
         isOn = false;
     }
     //get methods
     public String getcpuId() {
         return cpuId;
     } 
-
     public int getRamVersion() {
         return ramVersion;
-    
     }
     public boolean getPCState() {
         return isOn;    
     }
+    
     //set methods
     public void setCpuId(String newCpuId) {
         cpuId = newCpuId;
@@ -29,12 +31,18 @@ public class Computer {
         ramVersion = newRamVersion;
 
     }
+    //switch isOn value from false to true and backwards
     public void switchPc() {
         if (isOn) {
             isOn = false;
         } else {
             isOn = true;
         }
+    }
+    
+    //print methods
+    public void printCpuDetails() {
+        System.out.println("Procesador: " + cpuId + " | Version DDR " + ramVersion + " | Encendido: " + isOn);
     
     }
     
